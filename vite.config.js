2bb8a41213998834565a695d5bf9325f1cff98d5
jiha <zhsclq5324@naver.com>
2024-02-06 21:06:58 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test-deploy",
+  // base: "/test-deploy",
   css: {
     modules: {
       localsConvention: "camelCase",
